a2398afa6ec37fab39bdce3a62c67c3efb56442d
salvadorc94 <secg.1994@gmail.com>
2018-11-06 21:41:57 -0600

    estructura inicial

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <title>Bienvenido</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+  <div class="padre">
+  <div class="card">
+    <div class="container">
+      <h1 class="texto1">Iniciar Sesión</h1>
+      <form id="LoginForm">
+        <input type="text"  id="inputUser" placeholder="Usuario"><br>
+        <input type="password"  id="inputPass" placeholder="Contraseña"><br>
+        <button type="submit" id="Acceder">Acceder</button>
+        <button type="submit" id="Invitado">Invitado</button>
+      </form>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>

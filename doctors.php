31dbdea46587eed3f118eddc9b4281dd86014dba
salvadorc94 <secg.1994@gmail.com>
2018-11-13 13:58:17 -0600

    terminado estructura web del proyecto

--- doctors.php
+++ doctors.php
@@ -5,15 +5,65 @@
   <meta charset="utf-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <link href="css/guestStyle.css" rel="stylesheet" type="text/css"/>
+<link href="css/modalBox.css" rel="stylesheet" type="text/css"/>
 </head>
 <body>
+
+    <!-- The Modal -->
+<div id="myModal2" class="modal">
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="close" onclick="closeBox2();">&times;</span>
+    <p style="font-size:1vw">Editar Usuario</p>
+    <form id="EditMedicForm">
+      <input type="text"  id="editUser" placeholder="Usuario" value="valor de la base"><br>
+      <p>Dejar en blanco para no cambiar</p>
+      <label style="font-size:1vw">Contraseña vieja: </label> <input type="password"  id="oldPass" /><br>
+      <label style="font-size:1vw">Contraseña nueva: </label><input type="password"  id="newPass" /><br>
+      <input type="text" id="editNumber" placeholder="Número" value="valor de la base"/><br>
+      <input type="desc" id="editDesc" placeholder="Descripción" value="valor de la base"/><br>
+      <button type="submit" id="Editar">Guardar</button>
+    </form>
+  </div>
+</div>
+
+    <script>
+    // Get the modal
+    var modal = document.getElementById('myModal');
+    var modal2 = document.getElementById('myModal2');
+    function popBox(){
+      modal.style.display = "block";
+    }
+    function closeBox(){
+      modal.style.display = "none";
+      document.getElementById('AddMedicForm').reset();
+    }
+    function popBox2(){
+      modal2.style.display = "block";
+    }
+    function closeBox2(){
+      modal2.style.display = "none";
+      document.getElementById('EditMedicForm').reset();
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal || event.target == modal2) {
+            modal.style.display = "none";
+            modal2.style.display = "none";
+            document.getElementById('AddMedicForm').reset();
+            document.getElementById('EditMedicForm').reset();
+        }
+    }
+    </script>
 
   <div class="topnav">
     <a href="index.php">SALIR</a>
     <a href="doctors.php" class="active">DOCTORES</a>
     <a href="patients.php">INICIO</a>
-    <a href="#">doctor_name</a>
+    <a href="#" onclick="popBox2();">doctor_name</a>
   </div>
+  
 
   <div class="main">
     <div class="padre">
--- doctorsAdmin.php
+++ doctorsAdmin.php
@@ -5,15 +5,80 @@
   <meta charset="utf-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <link href="css/guestStyle.css" rel="stylesheet" type="text/css"/>
+    <link href="css/modalBox.css" rel="stylesheet" type="text/css"/>
 </head>
 <body>
+    <!-- The Modal -->
+  <div id="myModal" class="modal">
+    <!-- Modal content -->
+    <div class="modal-content">
+      <span class="close" onclick="closeBox();">&times;</span>
+      <h1 style="font-size:3vw">Crear medico</h1>
+      <form id="AddMedicForm">
+        <input type="text"  id="addUser" placeholder="Usuario"><br>
+        <input type="text"  id="addPass" placeholder="DummyPass"/><br>
+        <input type="text" id="addNumber" placeholder="Número"/><br>
+        <input type="desc" id="addDesc" placeholder="Descripción"/><br>
+        <button type="submit" id="Guardar">Guardar</button>
+      </form>
+    </div>
+</div>
+
+<!-- The Modal -->
+<div id="myModal2" class="modal">
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="close" onclick="closeBox2();">&times;</span>
+    <p style="font-size:1vw">Editar Usuario</p>
+    <form id="EditMedicForm">
+      <input type="text"  id="editUser" placeholder="Usuario" value="valor de la base"><br>
+      <p>Dejar en blanco para no cambiar</p>
+      <label style="font-size:1vw">Contraseña vieja: </label> <input type="password"  id="oldPass" /><br>
+      <label style="font-size:1vw">Contraseña nueva: </label><input type="password"  id="newPass" /><br>
+      <input type="text" id="editNumber" placeholder="Número" value="valor de la base"/><br>
+      <input type="desc" id="editDesc" placeholder="Descripción" value="valor de la base"/><br>
+      <button type="submit" id="Editar">Guardar</button>
+    </form>
+  </div>
+</div>
+
+    <script>
+    // Get the modal
+    var modal = document.getElementById('myModal');
+    var modal2 = document.getElementById('myModal2');
+    function popBox(){
+      modal.style.display = "block";
+    }
+    function closeBox(){
+      modal.style.display = "none";
+      document.getElementById('AddMedicForm').reset();
+    }
+    function popBox2(){
+      modal2.style.display = "block";
+    }
+    function closeBox2(){
+      modal2.style.display = "none";
+      document.getElementById('EditMedicForm').reset();
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal || event.target == modal2) {
+            modal.style.display = "none";
+            modal2.style.display = "none";
+            document.getElementById('AddMedicForm').reset();
+            document.getElementById('EditMedicForm').reset();
+        }
+    }
+    </script>
+
 
   <div class="topnav">
     <a href="index.php">SALIR</a>
-    <a href="#creardoctor">AÑADIR DOCTOR</a>
+    <a href="#creardoctor" onclick="popBox();">AÑADIR DOCTOR</a>
     <a href="doctorsAdmin.php" class="active">DOCTORES</a>
     <a href="admin.php">INICIO</a>
-    <a href="#">doctor_name</a>
+    <a href="#" onclick="popBox2();">doctor_name</a>
   </div>
 
   <div class="main">
